7b152db88f31b4e74308fb52224075f8997d3990
Rodrigo Severo <rodrigoseverodev@gmail.com>
2023-07-27 18:39:16 -0400

    update teste

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\LocatarioController;
+use App\Http\Controllers\FiadorController;
+use App\Http\Controllers\ProprietarioController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,8 +12,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -17,6 +21,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/proprietarios/create', [App\Http\Controllers\ProprietarioController::class, 'create'] )->name('proprietarios.create');
+Route::post('/proprietarios/store', [App\Http\Controllers\ProprietarioController::class, 'store'] )->name('proprietarios.store');;;
+
+Route::get('/locatarios/create',[App\Http\Controllers\LocatarioController::class, 'create'])->name('locatarios.create');
+Route::post('/locatarios/store',[App\Http\Controllers\LocatarioController::class, 'store'])->name('locatarios.store');
+
+Route::get('/fiadores/create',  [App\Http\Controllers\FiadorController::class, 'create'])->name('fiadores.create');
+Route::post('/fiadores/store',  [App\Http\Controllers\FiadorController::class, 'store'])->name('fiadores.store');
+
+
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/proprietarios', [App\Http\Controllers\ProprietarioController::class, 'index'] )->name('proprietarios.index');
+Route::get('/locatario', [App\Http\Controllers\LocatarioController::class, 'index'] )->name('locatarios.index');
+Route::get('/locatario/edit', [App\Http\Controllers\LocatarioController::class, 'edit'] )->name('locatarios.edit');
+Route::delete('/locatario/destroy', [App\Http\Controllers\LocatarioController::class, 'destroy'] )->name('locatarios.destroy');
+Route::get('/fiadores', [App\Http\Controllers\FiadorController::class, 'index'] )->name('fiadores.index');
+
+
